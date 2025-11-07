c487198252a62b59109467f8cb6cd0698d3a3e3f
Gerald O. Calderon <geraldcalderonofficial@gmail.com>
2025-11-07 14:12:32 +0800

    update file

--- Program.cs
+++ Program.cs
@@ -10,9 +10,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
